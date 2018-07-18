846ec163177aab7c3d7d9a9caa43838ff44f1455
Robert Tate <robertjosephtate@gmail.com>
2018-07-18 11:46:48 -0500

    removed API key, resetting

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyDveTnWGDRmOF3g4mzCtS2kRunFUqDjM1g",
+    url: "https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts",
     success: function(result){
         console.log(result.items["1"].content);
     }
