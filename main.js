891824b1251d523df81e772a6eb64d5661b7712f
Robert Tate <robertjosephtate@gmail.com>
2018-07-18 11:41:33 -0500

    added bulma framework

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $.ajax({
     url: "https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyDveTnWGDRmOF3g4mzCtS2kRunFUqDjM1g",
     success: function(result){
-        console.log(result.items["3"].content);
+        console.log(result.items["1"].content);
     }
 });
 
