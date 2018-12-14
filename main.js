f91c1b677fc026b9404538e4f7e0c714edfb242a
Robert Tate <robertjosephtate@gmail.com>
2018-12-13 22:21:02 -0600

    WIP

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ class Blog {
     constructor() {
     }
 
-    getArticle(articleNumber) {
+    set article(articleNumber) {
         fetch("https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8")
         .then(response => response.json())
-        .then(result => console.log(result.items[articleNumber].content));
+        .then((result) => {
+            console.log(result.items[articleNumber].content);
+            this._article = result.items[articleNumber].content
+        });
     };
+
+    get article() {
+        return this._article;
+    }
+
+
 }
 
 let blog = new Blog;
 
-blog.getArticle(1)
+blog.article = 3;
 
-let body = document.querySelectorAll('[data-test]');
+console.log(blog);
+
+
+
+// let body = document.querySelectorAll('[data-test]');
 
 
 // .then(result => this._article = result.items[articleNumber].content);
