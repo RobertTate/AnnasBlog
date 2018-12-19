4c96b04ffb7594c0754ab527a464bb254ecbceda
Bobby Tate <bobby.tate@daveramsey.com>
2018-12-19 12:24:43 -0600

    WIP

--- main.js
+++ main.js
@@ -1,56 +1,12 @@
-class Blog {
-    constructor() {
-    }
-
-    set article(articleNumber) {
-        fetch("https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8")
-        .then(response => response.json())
-        .then((result) => {
-            console.log(result.items[articleNumber].content);
-            this._article = result.items[articleNumber].content
-        });
-    };
-
-    get article() {
-        return this._article;
-    }
-
-
+async function fetchArticle(articleNumber) {
+    await fetch("https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8")
+    .then(response => response.json())
+    .then((result) => {
+        let parent = document.querySelector('[data-parent]');
+        let sibling = document.querySelectorAll('[data-sibling]');
+        let currentArticle = document.createRange().createContextualFragment(result.items[articleNumber].content);
+        parent.insertBefore(currentArticle, sibling[0]);
+    });
 }
 
-let blog = new Blog;
-
-blog.article = 3;
-
-console.log(blog);
-
-
-
-// let body = document.querySelectorAll('[data-test]');
-
-
-// .then(result => this._article = result.items[articleNumber].content);
-
-
-
-
-// $.ajax({
-//     url: "https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8",
-//     success: function (result) {
-//         console.log(result);
-        
-//     }
-// });
-
-
-
-// $.ajax({
-//     url: "https://www.googleapis.com/blogger/v3/blogs/2500055890074266959/posts?key=AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8",
-//     success: function(result){
-//         console.log(result.items["1"].content);
-//     }
-// });
-
-
-
-// AIzaSyAsEUDdev13GZ1hOa7vMnjDdCL8xmph_i8
+fetchArticle(1);
